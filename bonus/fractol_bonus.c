426ad8ba6bb79acc4d9536aa2d417ff1f9bf3f7e
Hugo <Hugoabouda@gmail.com>
2024-09-12 18:16:31 +0200

    ckghvrebjkerjlrk

--- bonus/fractol_bonus.c
+++ bonus/fractol_bonus.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:07:39 by habouda           #+#    #+#             */
-/*   Updated: 2024/09/12 17:33:50 by habouda          ###   ########.fr       */
+/*   Updated: 2024/09/12 18:15:29 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,37 @@ void	draw_fractal(t_fractal *fractal, char *argv)
 {		
 	if (ft_strncmp(argv, "julia", 6) == 0)
 	{
-		if (!fractal->cx && !fractal->cy)
-		{
-			fractal->cx = ft_atof(argv[2]) ;
-			fractal->cy = ft_atof(argv[3]);
-		}
 		draw_julia(fractal);
 	}
 	else if (ft_strncmp(argv, "mandle", 7) == 0)
 		draw_mandle(fractal);
-	else if (ft_strncmp(argv, "ship", 5) == 0)
+	else if (strncmp(argv, "ship", 5) == 0)
 		draw_ship(fractal);
 	mlx_put_image_to_window(fractal->mlx, fractal->window, fractal->image, 0,
 	0);
 }
 
-
 int	main(int argc, char *argv[])
 {
 	t_fractal	*fractal;
 
-	argc = argc + 0;
 	fractal = malloc(sizeof(t_fractal));
 	init_fractol(fractal);
+	argc = argc + 0;
+	if (ft_strncmp(argv[1], "julia", 6) == 0)
+	{
+		fractal->name = "julia";
+		fractal->cx = atof(argv[2]);
+		fractal->cy = atof(argv[3]);
+	}
+	if (ft_strncmp(argv[1], "mandle", 7) == 0)
+		fractal->name = "mandle";
+	if (ft_strncmp(argv[1], "ship", 5) == 0)
+		fractal->name = "ship";
 	init_mlx(fractal);
 	mlx_key_hook(fractal->window, key_hook, fractal);
 	mlx_mouse_hook(fractal->window, mouse_hook, fractal);
 	mlx_hook(fractal->window, 17, 0L, exit_fractal, fractal);
-	draw_fractal(fractal, argv[1]);
+	draw_fractal(fractal, fractal->name);
 	mlx_loop(fractal->mlx);
-	
 }
--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:07:39 by habouda           #+#    #+#             */
-/*   Updated: 2024/09/12 17:36:43 by habouda          ###   ########.fr       */
+/*   Updated: 2024/09/12 18:12:02 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,10 @@ void	draw_fractal(t_fractal *fractal, char *argv)
 {		
 	if (ft_strncmp(argv, "julia", 6) == 0)
 	{
-		if (!fractal->cx && !fractal->cy)
-		{
-			fractal->cx = -0.744 ;
-			fractal->cy = 0.148;
-		}
 		draw_julia(fractal);
 	}
 	else if (ft_strncmp(argv, "mandle", 7) == 0)
 		draw_mandle(fractal);
-
 	mlx_put_image_to_window(fractal->mlx, fractal->window, fractal->image, 0,
 	0);
 }
@@ -35,14 +29,23 @@ int	main(int argc, char *argv[])
 {
 	t_fractal	*fractal;
 
-	argc = argc + 0;
 	fractal = malloc(sizeof(t_fractal));
 	init_fractol(fractal);
+	argc = argc + 0;
+	if (ft_strncmp(argv[1], "julia", 6) == 0)
+	{
+		fractal->name = "julia";
+		fractal->cx = atof(argv[2]);
+		fractal->cy = atof(argv[3]);
+	}
+	if (ft_strncmp(argv[1], "mandle", 7) == 0)
+	{
+		fractal->name = "mandle";
+	}
 	init_mlx(fractal);
 	mlx_key_hook(fractal->window, key_hook, fractal);
 	mlx_mouse_hook(fractal->window, mouse_hook, fractal);
 	mlx_hook(fractal->window, 17, 0L, exit_fractal, fractal);
-	draw_fractal(fractal, argv[1]);
+	draw_fractal(fractal, fractal->name);
 	mlx_loop(fractal->mlx);
-	
 }
